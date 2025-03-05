6dfb3681868e436df388087765b5b08f9a113b14
c1oud99 <1imepie.c1oud9@gmail.com>
2025-03-05 17:00:58 +0900

    update

--- 5532.py
+++ 5532.py
@@ -3,20 +3,4 @@ A = int(input())
 B = int(input())
 C = int(input())
 D = int(input())
-
-E, F = 0, 0
-if A % C != 0:
-    E = (A // C) + 1
-else:
-    E = A // C
-if B % D != 0:
-    F = (B // D) + 1
-else:
-    F = B // D
-
-if E > F:
-    print(L - E)
-elif E < F:
-    print(L - F)
-else:
-    print(L - E)
+print(L - max(-(-A // C), -(-B // D)))
