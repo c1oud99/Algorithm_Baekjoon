173d1542e271bd8b002901b8791a2cfbef2d3cd6
c1oud99 <1imepie.c1oud9@gmail.com>
2025-03-08 23:59:33 +0900

    update

---
+++ 2740.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+N, M = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(N)]
+M, K = map(int, input().split())  #
+B = [list(map(int, input().split())) for _ in range(M)]
+
+AB = np.dot(A, B)
+
+for i in range(N):
+    print(*AB[i])
