9fb8666ca103b6e9b5fe927525945d62192c4520
c1oud99 <1imepie.c1oud9@gmail.com>
2025-03-09 14:53:56 +0900

    add

--- 2740.py
+++ 2740.py
@@ -1,11 +1,13 @@
-import numpy as np
-
 N, M = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(N)]
 M, K = map(int, input().split())  #
 B = [list(map(int, input().split())) for _ in range(M)]
 
-AB = np.dot(A, B)
-
+AB = [[0]*(K) for _ in range(N)]
 for i in range(N):
-    print(*AB[i])
+    for j in range(K):
+        for k in range(M):
+            AB[i][j] += A[i][k] * B[k][j]
+
+for row in AB:
+    print(*row)
