3ce803da23bdd89659a11ca9a17b9a14da424924
c1oud99 <1imepie.c1oud9@gmail.com>
2025-03-15 15:01:08 +0900

    add

--- 1026.py
+++ 1026.py
@@ -1,5 +1,4 @@
 N = int(input())
 A = sorted(map(int, input().split()))
 B = sorted(map(int, input().split()), reverse=True)
-S = sum(a * b for a, b in zip(A, B))
-print(S)
+print(sum(a * b for a, b in zip(A, B)))
